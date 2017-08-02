3c538f626c30cff91136a4e0a4b5f5e6c11b75df
Zhenglai Zhang <zhenglai.zhang@quadas.com>
2017-08-02 18:04:27 +0800

    color space processing

---
+++ exp/img_basic_process_004_color_space.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+
+# 对于 BGR$Gray 的转换，我们要使用的 flag 就是 cv2.COLOR_BGR2GRAY。
+# 同样对于 BGR$HSV 的转换，我们用的 flag 就是 cv2.COLOR_BGR2HSV。
+
+# 在 OpenCV 的 HSV 格式中， H（色彩/色度）的取值范围是 [0， 179]，
+# S（饱和度）的取值范围 [0， 255]， V（亮度）的取值范围 [0， 255]。但是不
+# 同的软件使用的值可能不同。所以当你需要拿 OpenCV 的 HSV 值与其他软
+# 件的 HSV 值进行对比时，一定要记得归一化。
+flags = [f for f in dir(cv2) if f.startswith('COLOR_')]
+print(flags)
+
+# 在 HSV 颜色空间中要比在 BGR 空间中更容易表示一个特定颜色。
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # get each frame
+    ret, frame = cap.read()
+
+    # to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # set blue threshold
+    lower_blue = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    cv2.imshow('res', res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
